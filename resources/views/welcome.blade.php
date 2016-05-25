9b15a50f4916156f7e772080bfa5e65afebfa1dc
LuaxY <yann.guineau@voidmx.net>
2016-05-25 11:19:39 +0200

    Image upload #1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,45 +1,31 @@
 <!DOCTYPE html>
-<html>
-    <head>
-        <title>upimgs.net</title>
-
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-
-        <style>
-            html, body {
-                height: 100%;
-            }
-
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 96px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="container">
-            <div class="content">
-                <div class="title">upimgs.net</div>
-            </div>
-        </div>
-    </body>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+    <title>upimgs.net</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	{!! Html::favicon('images/favicon.ico') !!}
+    {!! Html::style('css/pomf.css') !!}
+</head>
+<body>
+<div class="container">
+    <div class="jumbotron">
+    	<h1 onclick="moon()" id="ohayou">upimgs</h1>
+    	<p class="lead">Limit is 100MiB</p>
+        <noscript>
+            <p class="alert alert-error"><strong>Enable JavaScript</strong> if you wanna use this site (damn the previous error was rude)</p>
+        </noscript>
+        <p id="no-file-api" class="alert alert-error">
+            <strong>This site requires some cool Web 2.0 stuff</strong> Install the latest<a href="http://firefox.com/">Firefox</a> or <a href="http://chrome.google.com/">Chrome</a> and come back &lt;3
+        </p>
+        <a href="javascript:;" id="upload-btn" class="btn">Select <span>or drop</span> file(s)</a>
+        <input type="file" id="upload-input" name="files[]" multiple data-max-size="100MiB">
+        <ul id="upload-filelist"></ul>
+    </div>
+    {!! Html::script('js/zepto.js') !!}
+    {!! Html::script('js/cheesesteak.js') !!}
+    {!! Html::script('js/cabinet.js') !!}
+    {!! Html::script('js/pomf.js') !!}
+</div>
+</body>
 </html>
